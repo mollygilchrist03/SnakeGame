a4e973fd609440bfc6165e9e3d13c717a5838de0
Molly Gilchrist <mgilchrist@my.gcu.edu>
2025-01-31 15:56:41 -0700

    Implemented Position class

--- SnakeGameClassLibrary/Position.cs
+++ SnakeGameClassLibrary/Position.cs
@@ -8,5 +8,13 @@ namespace SnakeGameClassLibrary
 {
     public class Position
     {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
     }
 }
